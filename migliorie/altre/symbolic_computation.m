054493eebf380a73eb36f292c28ddfbd5dfad458
matt0710 <50274745+matt0710@users.noreply.github.com>
2021-01-03 18:57:06 +0100

    Add files via upload

---
+++ migliorie/altre/symbolic_computation.m
@@ -0,0 +1,8 @@
+function [C_sym,x_sym] = symbolic_computation(a1, b1)
+%   Compute symbolic calculation of transformation matrix from
+%rectangular form to polar form
+theta_sym = [(sqrt(a1^2 + b1^2)), atan2(b1, a1)];
+x_sym = [a1, b1];
+C_sym = simplify(jacobian(theta_sym,x_sym));
+
+end
